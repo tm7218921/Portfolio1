7329a528a0a37f1b95232fa91a54ce3d8c39c416
Tanmay Mandal <142010031+tm7218921@users.noreply.github.com>
2025-12-27 18:30:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+
 // Pages
 import ProfileSection from "./pages/ProfileSection";
 import About from "./pages/About";
@@ -12,6 +13,7 @@ import WorkshopsAndHackathons from "./pages/WorkshopsAndHackathons";
 import Projects from "./pages/Projects";
 import TechnicalBlog from "./pages/TechnicalBlog";
 import Contact from "./pages/Contact";
+
 import { Helmet } from "react-helmet";
 import "./index.css";
 
@@ -20,22 +22,29 @@ function App() {
     <Router>
       <Helmet>
         <title>Tanmay Mandal - AI & Data Science Portfolio</title>
-        <meta name="description" content="Portfolio of Tanmay Mandal - AI & Data Science Engineer" />
+        <meta
+          name="description"
+          content="Portfolio of Tanmay Mandal - AI & Data Science Engineer"
+        />
       </Helmet>
+
       <Header />
+
       <Routes>
         <Route path="/" element={<ProfileSection />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/work-experience" element={<WorkExperience />} />
-        <Route path="/workshops" element={<WorkshopsAndHackathons />} />
         <Route path="/academic-activities" element={<AcademicActivities />} />
+        <Route path="/workshops" element={<WorkshopsAndHackathons />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<TechnicalBlog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
     </Router>
   );
 }
+
 export default App;
