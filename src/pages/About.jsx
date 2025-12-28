410d3ee3bdb8e91cdc74eeecfce824c7fda3013e
Tanmay Mandal <142010031+tm7218921@users.noreply.github.com>
2025-12-28 23:21:46 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       <h2>About Me</h2>
       <p>
       I’m Tanmay, someone who enjoys working at the intersection of strategy, design, and new technology.
-      I’m currently pursuing engineering in AI & Data Science at Datta Meghe College (CGPA: 7.32, 2022–2026).
+      I’m currently pursuing engineering in AI & Data Science at Datta Meghe College (CGPA: 7.54, 2022–2026).
       I’ve worked on projects ranging from real-time AI systems to scalable web applications, and I love solving challenging problems through clean code and collaborative, impact-driven work.
       </p>
     </section>
