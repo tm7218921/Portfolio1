2ce3f70615a9881c59b4758a819811a97cd577af
Tanmay Mandal <142010031+tm7218921@users.noreply.github.com>
2026-01-02 19:10:58 +0530

    Update ProfileSection.jsx

--- src/pages/ProfileSection.jsx
+++ src/pages/ProfileSection.jsx
@@ -11,8 +11,8 @@ export default function ProfileSection() {
         <h1 className="profile__name">Tanmay Vikas Mandal</h1>
         <p className="profile__role">Software Engineer</p>
         <div className="profile__contact">
-          <a href="mailto:aids.61.tanmaymandal@gmail.com">aids.61.tanmaymandal@gmail.com</a> |
-          <a href="tel:+917385122101">+91-7385122101</a>
+          <a href="mailto:aids.61.tanmaymandal@gmail.com" target="_blank" rel="noopener noreferrer">aids.61.tanmaymandal@gmail.com</a> |
+          <a href="tel:+917385122101" target="_blank" rel="noopener noreferrer">+91-7385122101</a>
         </div>
         <div className="profile__links">
           <a href="https://github.com/tm7218921" target="_blank" rel="noopener noreferrer">GitHub</a>
